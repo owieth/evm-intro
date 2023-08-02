1086a514e92e2fd97f1e5c3b2ccdad25df4f7c30
Olivier <olivier.winkler@icloud.com>
2023-08-02 16:49:27 +0200

    feat(contracts): update Coin.sol contract

--- src/Coin.sol
+++ src/Coin.sol
@@ -1,4 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+///
+///  ██████╗ ██████╗ ██╗███╗   ██╗
+/// ██╔════╝██╔═══██╗██║████╗  ██║
+/// ██║     ██║   ██║██║██╔██╗ ██║
+/// ██║     ██║   ██║██║██║╚██╗██║
+/// ╚██████╗╚██████╔╝██║██║ ╚████║
+///  ╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝
+///
+/// @title EVM INTRO - MyCoin
+/// @author https://github.com/owieth/ (Olivier Winkler)
+/// @notice EVM Intro Demo
+/// @custom:security-contact abc@gmail.com
+contract MyCoin is ERC20 {
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    constructor() ERC20("MyCoin", "MCK") {}
+
+    /*//////////////////////////////////////////////////////////////
+                                PUBLIC
+    //////////////////////////////////////////////////////////////*/
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
